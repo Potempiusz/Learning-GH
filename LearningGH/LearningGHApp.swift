ccfb8a61efe82d2e4fae2f7fa6ce1637d5320562
Mateusz Potępa <mateusz.potepa@gmail.com>
2023-01-26 22:01:38 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Mateusz Potępa on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
